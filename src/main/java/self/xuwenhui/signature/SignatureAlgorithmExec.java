cd68ce7a649908319308c31415df5a1ed26a2412
徐文辉 <wenh.xu001@bkjk.com>
2020-06-02 10:05:07 +0800

    add a method support bytes verify

--- src/main/java/self/xuwenhui/signature/SignatureAlgorithmExec.java
+++ src/main/java/self/xuwenhui/signature/SignatureAlgorithmExec.java
@@ -47,9 +47,24 @@ public class SignatureAlgorithmExec {
      */
     public static boolean verify(String data, String signatureStr, String publicKeyStr,
                                  SignatureAlgorithmEnum signatureAlgorithm) throws Exception {
+        return verify(data.getBytes(), signatureStr, publicKeyStr, signatureAlgorithm);
+    }
+
+    /**
+     * 验签
+     *
+     * @param data               需要验证的数据(上面方法的参数 data)
+     * @param signatureStr       签名的字符串(上面方法的返回值)
+     * @param publicKeyStr       预先分配好的公钥
+     * @param signatureAlgorithm 签名算法
+     * @return true表示成功，false表示失败
+     * @throws Exception
+     */
+    public static boolean verify(byte[] data, String signatureStr, String publicKeyStr,
+            SignatureAlgorithmEnum signatureAlgorithm) throws Exception {
         Signature signature = Signature.getInstance(signatureAlgorithm.name());
         signature.initVerify(EncryptAlgorithm.generatePublicKeyFrom(publicKeyStr, signatureAlgorithm.getAlgorithm()));
-        signature.update(data.getBytes());
+        signature.update(data);
         return signature.verify(EncryptAlgorithm.base64DecodeString(signatureStr));
     }
 }
