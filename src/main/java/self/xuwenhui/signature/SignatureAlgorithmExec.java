0debc08d948ff2dd4cbfa42d7242aad208715f7b
wenh.xu001 <wenh.xu001@bkjk.com>
2020-06-02 08:46:08 +0800

    add a method support bytes signature

--- src/main/java/self/xuwenhui/signature/SignatureAlgorithmExec.java
+++ src/main/java/self/xuwenhui/signature/SignatureAlgorithmExec.java
@@ -3,6 +3,23 @@ package self.xuwenhui.signature;
 import java.security.Signature;
 
 public class SignatureAlgorithmExec {
+    /**
+     * 签名
+     *
+     * @param data               待签名的字节数组
+     * @param privateKeyStr      预先分配好的私钥
+     * @param signatureAlgorithm 签名算法
+     * @return 签名完成后的字符串数据
+     * @throws Exception
+     */
+    public static String sign(byte[] data, String privateKeyStr, SignatureAlgorithmEnum signatureAlgorithm)
+            throws Exception {
+        Signature signature = Signature.getInstance(signatureAlgorithm.name());
+        signature.initSign(EncryptAlgorithm.generatePrivateKeyFrom(privateKeyStr, signatureAlgorithm.getAlgorithm()));
+        signature.update(data);
+        byte[] bytes = signature.sign();
+        return EncryptAlgorithm.base64EncodeToString(bytes);
+    }
 
     /**
      * 签名
